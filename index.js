5a365df01a602c0f97bc899cfed86c92ad33eb81
Noor Piash <noorpiash1234@gmail.com>
2025-07-18 04:02:53 +0600

    68-3

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ async function run() {
 
     app.post("/users", async (req, res) => {
       const user = req.body;
+      const query = { email: user.email };
+      const existingUser = await userCollection.findOne(query);
+      if (existingUser) {
+        return res.send({ message: "User already exists", insertId: null });
+      }
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
