35a6270367563f1e903423c3cc973867995849e2
Noor Piash <noorpiash1234@gmail.com>
2025-08-09 02:16:49 +0600

    71-4

--- index.js
+++ index.js
@@ -234,6 +234,37 @@ async function run() {
       res.send({ paymentResult, deleteResult });
     });
 
+    app.get("/admin-stats", verifyToken, verifyAdmin, async (req, res) => {
+      const users = await userCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      // const payments = await paymentCollection
+      //   .find({ status: "paid" })
+      //   .toArray();
+      // const revenue = payments.reduce(
+      //   (prev, payment) => prev + payment.price,
+      //   0
+      // );
+
+      const result = await paymentCollection
+        .aggregate([
+          {
+            $group: {
+              _id: null,
+              totalRevenue: {
+                $sum: "$price",
+              },
+            },
+          },
+        ])
+        .toArray();
+
+      const revenue = result.length > 0 ? result[0].totalRevenue : 0;
+
+      res.send({ users, products, orders, revenue });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
